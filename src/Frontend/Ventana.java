717e124a5898942271db89f0ca037eded3277a4c
Reyes06 <eddyarnoldo06@gmail.com>
2021-01-27 17:27:19 -0600

    Retoques graficos de la interfaz

--- src/Frontend/Ventana.java
+++ src/Frontend/Ventana.java
@@ -46,6 +46,7 @@ public class Ventana extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Analisis reporte de ingresos - Televida");
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -153,7 +154,7 @@ public class Ventana extends javax.swing.JFrame {
 
         jTabbedPane1.addTab("Costa Rica", jScrollPane5);
 
-        jButton1.setText("jButton1");
+        jButton1.setText("Procesar cobros Humberto Chitay");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
